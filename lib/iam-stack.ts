4a132e8c03987962be91c488d748765cd013b6d2
Ignacio Fuentes <ignaciofuentes@outlook.com>
2024-04-23 16:52:23 -0400

    init commit

---
+++ lib/iam-stack.ts
@@ -0,0 +1,19 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Role } from "aws-cdk-lib/aws-iam";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class IamStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+    let role = new Role(this, "CDKIAMRole111", {
+      assumedBy: new cdk.aws_iam.ServicePrincipal("ec2.amazonaws.com"),
+      managedPolicies: [
+        cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "AdministratorAccess"
+        ),
+      ],
+    });
+  }
+}
