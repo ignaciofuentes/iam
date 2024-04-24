cd8693fcea8f3042e0e4b6e9ebe3aff4c7dad6c3
Ignacio Fuentes <ignaciofuentes@outlook.com>
2024-04-24 11:22:34 -0400

    json based policy

--- lib/iam-stack.ts
+++ lib/iam-stack.ts
@@ -1,19 +1,28 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { Bucket } from "aws-cdk-lib/aws-s3";
-import { Role } from "aws-cdk-lib/aws-iam";
+import {
+  ManagedPolicy,
+  PolicyDocument,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
+import * as fs from "fs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class IamStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Read the policy document from a JSON file
+    const policyJson = fs.readFileSync("candidate-policy.json", "utf8");
+    const policyDocument = PolicyDocument.fromJson(JSON.parse(policyJson));
+
     let role = new Role(this, "MyAwesomeRole", {
-      assumedBy: new cdk.aws_iam.ServicePrincipal("ec2.amazonaws.com"),
-      managedPolicies: [
-        cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName(
-          "AdministratorAccess"
-        ),
-      ],
+      assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
+      inlinePolicies: {
+        myPolicy: policyDocument,
+      },
     });
   }
 }
